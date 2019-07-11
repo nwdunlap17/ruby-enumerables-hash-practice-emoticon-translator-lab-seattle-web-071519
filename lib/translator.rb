dec33c38c45d55857b5e6285a62c1ce6135df1c8
Nicholas Dunlap <pjnick-300@hotmail.com>
2019-07-11 22:28:40 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,16 @@
-# require modules here
-
+require 'yaml'
+  
 def load_library
+  database = YAML.load_file('emoticons.yml')
+  p database
+end
+
+def get_japanese_emoticon(face)
   # code goes here
 end
 
-def get_japanese_emoticon
+def get_english_meaning(face)
   # code goes here
 end
 
-def get_english_meaning
-  # code goes here
-end
+load_library
