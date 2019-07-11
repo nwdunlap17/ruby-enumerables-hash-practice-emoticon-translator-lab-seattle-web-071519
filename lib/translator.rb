87fc08a43b94341705e28b61b0505742a6ef4c96
Nicholas Dunlap <pjnick-300@hotmail.com>
2019-07-11 22:34:20 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,13 +7,20 @@ end
 
 def get_japanese_emoticon(face)
   database = load_library
-  database.each do |key,value|
-    p database[key][0]
+  database.each_key do |key|
+    if database[key][0] == face
+      return database[key][1]
+    end
   end
 end
 
 def get_english_meaning(face)
-  # code goes here
+  database = load_library
+  database.each_key do |key|
+    if database[key][1] == face
+      return key
+    end
+  end
 end
 
 data = load_library
